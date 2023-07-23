95c065c1e6a74f563bd51b87708c83e7b15ad385
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-23 17:17:05 +0400

    Added basic Dark Mode theme switcher ✅

--- Taskwise CLI/dashboard.py
+++ Taskwise CLI/dashboard.py
@@ -1,77 +1,22 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Label, Tabs
-
-TAB_NAMES = [
-    "Dashboard",
-    "To-Do List",
-    "TimeTables"
-]
+from textual.widgets import Header, Footer
 
 
-class TabsApp(App):
-    """Demonstrates the Tabs widget."""
+class StopwatchApp(App):
+    """A Textual app to manage stopwatches."""
 
-    CSS = """
-    Tabs {
-        dock: top;
-    }
-    Screen {
-        align: center middle;
-    }
-    Label {
-        margin:1 1;
-        width: 100%;
-        height: 100%;
-        background: $panel;
-        border: tall $primary;
-        content-align: center middle;
-    }
-    """
-
-    BINDINGS = [
-        ("a", "add", "Add tab"),
-        ("r", "remove", "Remove active tab"),
-        ("c", "clear", "Clear tabs"),
-    ]
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
-        yield Tabs(TAB_NAMES[0])
-        yield Label()
+        """Create child widgets for the app."""
+        yield Header()
         yield Footer()
 
-    def on_mount(self) -> None:
-        """Focus the tabs when the app starts."""
-        self.query_one(Tabs).focus()
-
-    def on_tabs_tab_activated(self, event: Tabs.TabActivated) -> None:
-        """Handle TabActivated message sent by Tabs."""
-        label = self.query_one(Label)
-        if event.tab is None:
-            # When the tabs are cleared, event.tab will be None
-            label.visible = False
-        else:
-            label.visible = True
-            label.update(event.tab.label)
-
-    def action_add(self) -> None:
-        """Add a new tab."""
-        tabs = self.query_one(Tabs)
-        # Cycle the names
-        TAB_NAMES[:] = [*TAB_NAMES[1:], TAB_NAMES[0]]
-        tabs.add_tab(TAB_NAMES[0])
-
-    def action_remove(self) -> None:
-        """Remove active tab."""
-        tabs = self.query_one(Tabs)
-        active_tab = tabs.active_tab
-        if active_tab is not None:
-            tabs.remove_tab(active_tab.id)
-
-    def action_clear(self) -> None:
-        """Clear the tabs."""
-        self.query_one(Tabs).clear()
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
 
 
 if __name__ == "__main__":
-    app = TabsApp()
+    app = StopwatchApp()
     app.run()
